d041a9c3af56570c01b507308f9cd11783574d09
Antonio J. García Lagar <aj@garcialagar.es>
2025-11-04 10:32:14 +0000

    Prepare development with frankenphp

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,21 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function handle(Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, bool $catch = true): Response
+    {
+        if ($this->isDebug() && function_exists('xdebug_connect_to_client')) {
+            \xdebug_connect_to_client();
+        };
+
+        return parent::handle($request, $type, $catch);
+    }
 }
