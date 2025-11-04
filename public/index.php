ef124ff0ae0df487b48da3864a4bd6503efeea20
Antonio J. García Lagar <aj@garcialagar.es>
2025-11-04 10:54:12 +0000

    Support worker mode

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-phpinfo();
+ if (!($_SERVER['FRANKENPHP_WORKER'] ?? false)) {
+    phpinfo();
+    exit(0);
+ }
+
+ignore_user_abort(true);
+
+$maxRequests = (int)($_SERVER['MAX_REQUESTS'] ?? 0);
+
+for ($nbRequests = 0; !$maxRequests || $nbRequests < $maxRequests; ++$nbRequests) {
+    $keepRunning = \frankenphp_handle_request(phpinfo(...));
+
+    gc_collect_cycles();
+
+    if (!$keepRunning) {
+        break;
+    }
+}
